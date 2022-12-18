d6d83043c8c599450b5c11bed1228f560b3a271c
VictorMatheus819 <victormatheus819@gmail.com>
2022-12-18 12:01:23 -0300

    continuacao de criacao de usuario

--- desafioAlpha/alfaapp/views.py
+++ desafioAlpha/alfaapp/views.py
@@ -13,21 +13,26 @@ def home(request):
     return render(request,'home.html',{"ativos":ativos})
 
 def create_user(request):
-   user = User.objects.create_user(request.POST)
-   if user.is_valid:
-     user.save() 
-     return render(request,'home.html',{"user":user})
+   email=request.POST.get('email',False)
+   password=request.POST.get('password',False)
+   username=request.POST.get('username',False)
+   print(username)
+   if username!=False:
+    user = User.objects.create_user(email =email, username=username, password =password)
+    user.save() 
+    return HttpResponseRedirect(reverse('login'))
    return render(request,'create_user.html')
 
-def create_new_ativo(request):
+def create_new_ativo(request,):
     ativo= AtivosForm(request.POST)
     if ativo.is_valid():
         sigla=request.POST.get('sigla',False)
-        search_interval=request.POST.get('search_interval',False)
+        if request.user.is_authenticated:
+            email = request.user.email
         modelAtivo=Ativos.objects.filter(sigla=sigla)
         if len(modelAtivo) == 0:
              ativo.save()
-             busca_valor_cotacao(sigla)
+             busca_valor_cotacao(sigla,email)
              return HttpResponseRedirect(reverse('index'))
         else:
              return render(request,'ativo_form.html',{"message":f'Ativo {sigla} já  existente em sua conta'})    
@@ -50,22 +55,21 @@ def update_ativo(request, id):
     
     return render(request,'update_ativo.html',{'ativo': ativo})
     
-def busca_valor_cotacao(sigla):
+def busca_valor_cotacao(sigla,email):
     url = f'https://brapi.dev/api/quote/{sigla}'
     r = requests.get(url)
     ativo=Ativos.objects.get(sigla=sigla)
-    
-    
+    print(email)
     data = r.json()
     cotacao=data['results'][0]['regularMarketPrice']
     ativo.nome=data['results'][0]['longName'] 
     ativo.cotacao=cotacao
     ativo.save()   
     if(ativo.preco_max<=cotacao):
-        preco_superior()
+        preco_superior(email)
     print(ativo.search_interval) 
     if(ativo.preco_min>=cotacao):
-        preco_inferior()    
+        preco_inferior(email)    
     requisicao = cycleRequisition(ativo.search_interval*60,busca_valor_cotacao,args=(sigla,))
     requisicao.start()
     if ativo.switch_off_thread == True:
@@ -74,8 +78,8 @@ def busca_valor_cotacao(sigla):
         ativo.delete()
         return HttpResponseRedirect(reverse('index'))
 
-def preco_superior():
+def preco_superior(email):
     print("valor superior")   
 
-def preco_inferior():
+def preco_inferior(email):
     print("valor inferior")    
